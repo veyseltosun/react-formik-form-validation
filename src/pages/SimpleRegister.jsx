c7902beb801532a4b8cb5fc6a91866fc198993b5
veyseltosun <postalarim1923@gmail.com>
2022-12-07 20:23:27 +0300

    hamoke.

--- src/pages/SimpleRegister.jsx
+++ src/pages/SimpleRegister.jsx
@@ -56,12 +56,12 @@ function SimpleRegister() {
             <label htmlFor='name' >Name</label>
             <input type="text" id="name" name="name" 
             onChange={formik.handleChange} value={formik.values.name} onBlur={formik.handleBlur}></input>
-            {formik.touched.name && formik.errors.name?<div>{formik.errors.name}</div>: null}
+            {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div>: null}
 
             <label htmlFor='email' >Email</label>
             <input type="text" id="email" name="email" onBlur={formik.handleBlur}
             onChange={formik.handleChange} value={formik.values.email}></input>
-            {formik.errors.email?<div>{formik.errors.email}</div>:null}
+            {formik.errors.email ? <div>{formik.errors.email}</div>:null}
             <label htmlFor='userName' >User Name</label>
             <input type="text" id="userName" name="userName" onBlur={formik.handleBlur}
             onChange={formik.handleChange} value={formik.values.userName}></input>
